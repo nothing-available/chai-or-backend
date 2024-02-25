3efee0a89d189def308e966bc2533c994b2e9902
nothing-available <smtkur31@gmail.com>
2024-02-26 00:52:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+// import express from 'express' // ES6--- both syntax are same
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
